acd554b74c85b446080450f78533dd8f50865971
BNanyingi <nanyingibarbara@gmail.com>
2020-10-05 12:34:03 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 import { BudgetTemListComponent } from './budget-tem-list/budget-tem-list.component';
+import { BudgetItemCardComponent } from './budget-tem-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddItemFormComponent,
-    BudgetTemListComponent
+    BudgetTemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
